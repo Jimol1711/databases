5fda12faadbac176dff434ec6337024d6ade45b5
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-10-16 14:20:26 -0300

    Se lee el csv

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -3,11 +3,11 @@ import psycopg2.extras
 import csv
 import re
 
-conn = psycopg2.connect(host=" cc3201 .dcc . uchile .cl",
-                        database=" cc3201 ",
-                        user=" cc3201 ",
-                        password="j'<3_cc3201 ", 
-                        port=" 5440 ")
+conn = psycopg2.connect(host="cc3201.dcc.uchile.cl",
+                        database="cc3201",
+                        user="cc3201",
+                        password="j'<3_cc3201", 
+                        port="5440")
 
 cursor = conn.cursor()
 
@@ -20,9 +20,12 @@ with open("Laboratorio_05_data.csv") as csvfile:
             continue
         id = row[0]
         name = row[1]
-        powerstats = [row[2],row[3],row[4],row[5],row[6],row[7]] # int, str, speed, dur, power, combat
-        biography_notaliases = [row[8],row[9],row[11],row[12],row[13],row[14]] # full name, alter egos, place of birth, first appearance, publisher, alignment
-
+        powerstats = row[2:8] # int, str, speed, dur, power, combat
+        biography_notaliases = row[8:10] + row[11:15] # full name, alter egos, place of birth, first appearance, publisher, alignment
+        biography_aliases = [row[10]] + row[27:46] # aliases 1 through 20
+        appearance = row[15:23] # gender, race, height_in, height_cm, weight_lb, weight_kg, eye color, hair color
+        work = row[23:25] # occupation, base
+        connections = row[25:27] # group affiliation, relatives
 
     conn.commit()
 
