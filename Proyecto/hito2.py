19a17ad6a691525198c4082a903022f1828e1626
Jimol1711 <juan.molina.l@ug.uchile.cl>
2023-10-31 23:46:18 -0300

    Avance proyecto

--- Proyecto/hito2.py
+++ Proyecto/hito2.py
@@ -20,8 +20,29 @@ with open("MoviesOnStreamingPlatforms_updated.csv", encoding="utf-8") as csvfile
         i+=1
         if i==1:
             continue
-        print(row)
+        id = row[0]
+        title = row[1]
+        year = row[2]
+        age = None
+        if row[3]!= "all" and row[3]:
+            age = int(row[3].split('+')[0]) 
+        elif row[3]=="all":
+            age = 0 
+        else:
+            age = None
+        IMDb = row[4] if row[4] else None
+        RT = int(row[5].split('%')[0])/100 if row[5] else None
+        runtime = row[15] if row[15] else None
 
-    conn.commit()
+def findOrInsert(table, name):
+    cursor.execute("select id from "+table+" where name=%s limit1", [name])
+    r = cursor.fetchone()
+    if(r):
+        return r[0]
+    else:
+        cursor.execute("insert into "+table+" (name) values (%s) returning id", [name])
+    return cursor.fetchone()[0]
+
+conn.commit()
 
 conn.close()
